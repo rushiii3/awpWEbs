d668d3161e1e7ae2b1c871ab13e513829033949e
HRUSHIKESH SHINDE <hrushiop@gmail.com>
2023-09-26 10:31:11 +0530

    Add files via upload

---
+++ WebSite2/Default.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void UserCustomValidate(object source, ServerValidateEventArgs args)
+    {
+        int val = Int32.Parse(args.Value.Substring(0, 1));
+        if (val == 4)
+        {
+            args.IsValid = true;
+        }
+        else
+        {
+            args.IsValid = false;
+        }
+    }
+}
---
+++ WebSite2/Footer.ascx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Footer : System.Web.UI.UserControl
+{
+   
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        lblFooter.Text = "This page was served at ";
+        lblFooter.Text += DateTime.Now.ToString();
+
+    }
+}
